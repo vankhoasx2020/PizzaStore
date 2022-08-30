ccd4f6d7182d0d124d720e3a8ddd54ff51111589
Văn Khoa Lê Chí <vankhoa.dx.2010@gmail.com>
2022-08-30 22:38:04 +0700

    Add project files.

---
+++ PizzaStore/Program.cs
@@ -0,0 +1,21 @@
+﻿using PizzaStore.Store;
+
+namespace PizzaStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaStore pizzaStore;
+
+            pizzaStore = new HUEPizzaStore();
+            pizzaStore.OrderPizza("cheese");
+
+            pizzaStore = new DNPizzaStore();
+            pizzaStore.OrderPizza("cheese");
+
+            pizzaStore = new HCMPizzaStore();
+            pizzaStore.OrderPizza("cheese");
+        }
+    }
+}
